87154db3671af5c873ba441cbe61e7c645688fd6
Dinukamalith <87223443+Dinukamalith@users.noreply.github.com>
2021-12-20 13:37:43 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import telebot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+TELEGRAM_TOKEN = '<5097104342:AAEuKw3O5TekOID2qabNOj2y-SOmTOzQX9c>'
+
+bot = telebot.TeleBot(TELEGRAM_TOKEN)
+
+def gen_markup():
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 2
+    markup.add(InlineKeyboardButton("Yes", callback_data="https://t.me/tech95lkofficial"),
+                               InlineKeyboardButton("No", callback_data="https://t.me/tech95lkofficial"))
+    return markup
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    if call.data == "https://t.me/tech95lkofficial":
+        bot.answer_callback_query(call.id, "Answer is Yes")
+    elif call.data == "https://t.me/tech95lkofficial":
+        bot.answer_callback_query(call.id, "Answer is No")
+
+@bot.message_handler(func=lambda message: True)
+def message_handler(message):
+    bot.send_message(message.chat.id, "Yes/no?", reply_markup=gen_markup())
+
+bot.polling(none_stop=True)
