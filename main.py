ceeaf9b2dabe9a4f8769a24e1f571d06e99c0afd
Dinukamalith <87223443+Dinukamalith@users.noreply.github.com>
2021-12-21 09:51:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,10 @@
-import os
-import telebot
-from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-TELEGRAM_TOKEN = ("5097104342:AAEuKw3O5TekOID2qabNOj2y-SOmTOzQX9c")
+from pyrogram import Client
 
-bot = telebot.TeleBot(TELEGRAM_TOKEN)
+app = Client("5097104342:AAEuKw3O5TekOID2qabNOj2y-SOmTOzQX9c")
 
-@bot.message_handler(commands=["start"])
-def send_welcome(message):
-  bot.reply_to(message, "welcome")
+with app:
 
-def gen_markup():
-    markup = InlineKeyboardMarkup()
-    markup.row_width = 2
-    markup.add(InlineKeyboardButton("Yes", callback_data="https://t.me/tech95lkofficial"),
-                               InlineKeyboardButton("No", callback_data="https://t.me/tech95lkofficial"))
-    return markup
-
-@bot.callback_query_handler(func=lambda call: True)
-def callback_query(call):
-    if call.data == "https://t.me/tech95lkofficial":
-        bot.answer_callback_query(call.id, "Answer is Yes")
-    elif call.data == "https://t.me/tech95lkofficial":
-        bot.answer_callback_query(call.id, "Answer is No")
-
-@bot.message_handler(func=lambda message: True)
-def message_handler(message):
-    bot.send_message(message.chat.id, "Yes/no?", reply_markup=gen_markup())
-
-bot.polling()
+    app.send_message("haskell", "hi")
+    
+    app.run()
