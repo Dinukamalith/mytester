01860bb267788718f4f1d2a57fd82f4035b4f8af
Dinukamalith <87223443+Dinukamalith@users.noreply.github.com>
2021-12-21 09:55:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 
-from pyrogram import Client
+
+from pyrogram import Client, filters
 
 app = Client("5097104342:AAEuKw3O5TekOID2qabNOj2y-SOmTOzQX9c")
 
-with app:
 
-    app.send_message("haskell", "hi")
-    
-    app.run()
+
+@app.on_message(filters.private)
+async def hello(client, message):
+    await message.reply_text(f"Hello {message.from_user.mention}")
+
+
+app.run()
